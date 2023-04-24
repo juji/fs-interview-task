b2000d4ac9cb9dd96adee630397413fddfb65439
juji <jujiyangasli@gmail.com>
2023-04-25 04:31:38 +0700

    refactor

--- api/src/lib/auth0.ts
+++ api/src/lib/auth0.ts
@@ -1,7 +1,7 @@
 import { auth, requiredScopes }  from 'express-oauth2-jwt-bearer';
 
 const audience = process.env.AUTH0_BASE_URL || 'http://localhost:3333'
-const issuerBaseURL = `${process.env.AUTH0_ISSUER_BASE_URL || 'https://jujiyangasli.auth0.com'}/`
+const issuerBaseURL = process.env.AUTH0_ISSUER_BASE_URL || 'https://jujiyangasli.auth0.com/'
 
 export const jwtCheck = auth({
     audience,
